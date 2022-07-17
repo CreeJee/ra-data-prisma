676173b2873424e26b74e92b9d065f1004167796
CreeJee <000as00060@gmail.com>
2022-07-17 21:44:51 +0900

    refactor: :recycle: prisma 4.0 base migration

--- packages/dataprovider/src/utils/makeIntrospectionOptions.ts
+++ packages/dataprovider/src/utils/makeIntrospectionOptions.ts
@@ -1,13 +1,15 @@
-import camelCase from "lodash/camelCase";
+import camelCase from "lodash-es/camelCase";
 import pluralize from "pluralize";
 import {
   CREATE,
   DELETE,
+  DELETE_MANY,
   GET_LIST,
   GET_MANY,
   GET_MANY_REFERENCE,
   GET_ONE,
   UPDATE,
+  UPDATE_MANY,
 } from "react-admin";
 import { Resource } from "../constants/interfaces";
 import { OurOptions, QueryDialect } from "../types";
@@ -15,19 +17,6 @@ import { makePrefixedFullName } from "./makePrefixedFullName";
 
 export const makeIntrospectionOptions = (options: OurOptions) => {
   const prefix = (s: string) => makePrefixedFullName(s, options?.aliasPrefix);
-
-  const mutationOperationNames: Record<QueryDialect, object> = {
-    "nexus-prisma": {
-      [CREATE]: (resource: Resource) => prefix(`createOne${resource.name}`),
-      [UPDATE]: (resource: Resource) => prefix(`updateOne${resource.name}`),
-      [DELETE]: (resource: Resource) => prefix(`deleteOne${resource.name}`),
-    },
-    typegraphql: {
-      [CREATE]: (resource: Resource) => prefix(`create${resource.name}`),
-      [UPDATE]: (resource: Resource) => prefix(`update${resource.name}`),
-      [DELETE]: (resource: Resource) => prefix(`delete${resource.name}`),
-    },
-  };
 
   return {
     operationNames: {
@@ -38,8 +27,13 @@ export const makeIntrospectionOptions = (options: OurOptions) => {
         prefix(`${pluralize(camelCase(resource.name))}`),
       [GET_MANY_REFERENCE]: (resource: Resource) =>
         prefix(`${pluralize(camelCase(resource.name))}`),
-
-      ...mutationOperationNames[options.queryDialect],
+      [CREATE]: (resource: Resource) => prefix(`createOne${resource.name}`),
+      [UPDATE]: (resource: Resource) => prefix(`updateOne${resource.name}`),
+      [DELETE]: (resource: Resource) => prefix(`deleteOne${resource.name}`),
+      [UPDATE_MANY]: (resource: Resource) =>
+        prefix(`updateMany${resource.name}`),
+      [DELETE_MANY]: (resource: Resource) =>
+        prefix(`deleteMany${resource.name}`),
     },
     exclude: undefined,
     include: undefined,
