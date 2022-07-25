3e7461383514db11bb9380c06e572d19f75bb8e6
CreeJee <000as00060@gmail.com>
2022-07-25 18:57:59 +0900

    revert prisma operator generation

--- packages/dataprovider/src/utils/makeIntrospectionOptions.ts
+++ packages/dataprovider/src/utils/makeIntrospectionOptions.ts
@@ -21,6 +21,25 @@ export const makeIntrospectionOptions = (
 ): Options["introspection"] => {
   const prefix = (s: string) => makePrefixedFullName(s, options?.aliasPrefix);
 
+  const mutationOperationNames: Record<QueryDialect, object> = {
+    "nexus-prisma": {
+      [CREATE]: (resource: IntrospectionType) =>
+        prefix(`createOne${resource.name}`),
+      [UPDATE]: (resource: IntrospectionType) =>
+        prefix(`updateOne${resource.name}`),
+      [DELETE]: (resource: IntrospectionType) =>
+        prefix(`deleteOne${resource.name}`),
+    },
+    typegraphql: {
+      [CREATE]: (resource: IntrospectionType) =>
+        prefix(`create${resource.name}`),
+      [UPDATE]: (resource: IntrospectionType) =>
+        prefix(`update${resource.name}`),
+      [DELETE]: (resource: IntrospectionType) =>
+        prefix(`delete${resource.name}`),
+    },
+  };
+
   return {
     operationNames: {
       [GET_LIST]: (resource: IntrospectionType) =>
@@ -31,16 +50,11 @@ export const makeIntrospectionOptions = (
         prefix(`${pluralize(camelCase(resource.name))}`),
       [GET_MANY_REFERENCE]: (resource: IntrospectionType) =>
         prefix(`${pluralize(camelCase(resource.name))}`),
-      [CREATE]: (resource: IntrospectionType) =>
-        prefix(`createOne${resource.name}`),
-      [UPDATE]: (resource: IntrospectionType) =>
-        prefix(`updateOne${resource.name}`),
-      [DELETE]: (resource: IntrospectionType) =>
-        prefix(`deleteOne${resource.name}`),
       [UPDATE_MANY]: (resource: IntrospectionType) =>
         prefix(`updateMany${resource.name}`),
       [DELETE_MANY]: (resource: IntrospectionType) =>
         prefix(`deleteMany${resource.name}`),
+      ...mutationOperationNames[options.queryDialect],
     },
     exclude: undefined,
     include: undefined,
